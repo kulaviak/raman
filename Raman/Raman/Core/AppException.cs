566cfa6b721089ce0f762511bb02d2453a8f2ee6
Jakub Kulaviak <ja.ku@seznam.cz>
2023-09-11 11:53:49 +0200

    In progress

---
+++ Raman/Raman/Core/AppException.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Raman.Core
+{
+    public class AppException : Exception
+    {
+        public AppException(string message) : base(message) {}
+
+        public AppException(string message, Exception innerException) : base(message, innerException) {}
+    }
+}
--- Raman/Raman/Core/FileReader.cs
+++ Raman/Raman/Core/FileReader.cs
@@ -7,20 +7,81 @@ namespace Raman.Core
 {
     public class FileReader
     {
-        public List<DataPoint> ReadFile(string filePath)
+        private readonly string _filePath;
+
+        private readonly List<string> _ignoredLines = new List<string>();
+
+        public FileReader(string filePath)
+        {
+            _filePath = filePath;
+        }
+        
+        public List<DataPoint> TryReadFile()
         {
             List<string> lines;
             try
             {
-                lines = File.ReadLines(filePath).ToList();
+                lines = File.ReadLines(_filePath).ToList().Where(x => !string.IsNullOrWhiteSpace(x)).ToList();
+                var ret = TryParseLines(lines);
+                return ret;
             }
             catch (Exception ex)
             {
-                throw new Exception($"Loading file {filePath} failed.", ex);
+                throw new Exception($"Loading file {_filePath} failed.", ex);
             }
+        }
+
+        private List<DataPoint> TryParseLines(List<string> lines)
+        {
+            var ret = new List<DataPoint>();
             try
             {
-                
+                foreach (var line in lines)
+                {
+                    var point = TryParseLine(line);
+                    if (point != null)
+                    {
+                        ret.Add(point);
+                    }
+                    else
+                    {
+                        _ignoredLines.Add(line);
+                    }
+                }
+                return ret;
+            }
+            catch (Exception e)
+            {
+                // ignore
+                return null;
+            }      
+        }
+
+        public static DataPoint TryParseLine(string line)
+        {
+            try
+            {
+                // split on white space or tab
+                var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                if (parts.Length >= 2)
+                {
+                    if (Decimal.TryParse(parts[0], out decimal x) && Decimal.TryParse(parts[1], out decimal y))
+                    {
+                        return new DataPoint(x, y);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                return null;
             }
         }
     }
