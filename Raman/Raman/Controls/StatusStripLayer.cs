ad465a793132b1aebb391e6b642c8e84e14b44f3
Jakub Kulaviak <jkulaviak@gmail.com>
2024-03-20 17:25:52 +0100

    Improve performance of status bar to not overload cpu

--- Raman/Raman/Controls/StatusStripLayer.cs
+++ Raman/Raman/Controls/StatusStripLayer.cs
@@ -7,19 +7,41 @@ public class StatusStripLayer : LayerBase
     private readonly AppStatusStrip statusStrip;
     
     private readonly CanvasPanel canvasPanel;
+    
+    private Point location;
+    
+    private Timer timer;
 
+    private const int DELAYED_RESPONSE_TIME_IN_MILISECONDS = 20;
+
+    private const double MAX_ALLOWED_DISTANCE_FOR_CLOSEST_SPECTRUM = 50;
+    
     public StatusStripLayer(CanvasCoordSystem coordSystem, AppStatusStrip statusStrip, CanvasPanel canvasPanel) : base(coordSystem)
     {
         this.statusStrip = statusStrip;
         this.canvasPanel = canvasPanel;
     }
-
-    private const double MAX_ALLOWED_DISTANCE_FOR_CLOSEST_SPECTRUM = 50;
     
     public override void HandleMouseMove(object sender, MouseEventArgs e)
     {
-        var spectrum = new ClosestSpectrumCalculator().GetClosestSpectrum(canvasPanel.VisibleSpectra, e.Location, CoordSystem, MAX_ALLOWED_DISTANCE_FOR_CLOSEST_SPECTRUM);
-        var positionText = GetPositionText(e.Location);
+        location = e.Location;
+        if (timer != null)
+        {
+            timer.Stop();
+        }
+        timer = new Timer {Interval = DELAYED_RESPONSE_TIME_IN_MILISECONDS};
+        timer.Tick += (_, _) =>
+        {
+            ShowStatusBarInfo();
+            timer.Stop();
+        }; 
+        timer.Start();
+    }
+    
+    private void ShowStatusBarInfo()
+    {
+        var spectrum = new ClosestSpectrumCalculator().GetClosestSpectrum(canvasPanel.VisibleSpectra, location, CoordSystem, MAX_ALLOWED_DISTANCE_FOR_CLOSEST_SPECTRUM);
+        var positionText = GetPositionText(location);
         var text = spectrum != null ? $"{positionText}     {spectrum.Name}" : positionText;
         statusStrip.ShowText(text);
     }
